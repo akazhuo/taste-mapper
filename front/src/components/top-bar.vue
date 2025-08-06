f603d84daeef63df60a17208233a481fd8528aae
akazhuo <akazhuo@qq.com>
2025-08-06 21:10:52 +0800

    feat: 新增 top-bar 搜索烂、修改样式

--- front/src/components/top-bar.vue
+++ front/src/components/top-bar.vue
@@ -1,17 +1,27 @@
 <template>
   <view class="top-bar">
-    <view class="menu">
-      <image mode="aspectFill" class="hamburger" src="../static/menu.svg" alt="" />
+    <view class="user-info">
+      <view class="address-info">
+        <img class="map-pin" src="@/static/map-pin.svg" alt="" />
+        <text class="text">{{ userAddress }}</text>
+      </view>
+      <view class="favorite">
+        <img class="heart" src="@/static/heart.svg" alt="" />
+      </view>
     </view>
-    <view class="address-info">{{ userAddress }}</view>
-    <view class="user-avatar">
-      <image mode="aspectFill"></image>
+    <view class="greeting">
+      <view class="content">用户，您好</view>
+      <view class="title">通过下方快速搜索、定位到喜爱的美食吧！</view>
+    </view>
+    <view class="search-input-wrap">
+      <img class="search-img" src="@/static/search.svg" alt="" />
+      <uni-easyinput v-model="query.keyword" class="searchinput" placeholder="早餐、汉堡、披萨、咖啡、奶茶..." :input-border="false"></uni-easyinput>
     </view>
   </view>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
   import { getUserLocation } from '@/composables/map.ts'
   import type { UserAddress } from '@/types/global.js'
   import { userStore } from '@/stores/user'
@@ -22,34 +32,100 @@
   getUserLocation().then((res: UserAddress) => {
     userAddress.value = res.address
   })
+  const query = reactive({ keyword: '' })
  
 </script>
 
 <style lang="scss" scoped>
+  @use '@/styles/colors.scss';
   .top-bar {
-    display: flex;
-    height: 58px;
-    padding: 0 25px;
-    align-items: center;
+    padding: 16px 0;
+    background: 0 0/cover url('@/static/Background.svg');
 
-    .menu {
-      .hamburger {
-        height: 24px;
-        width: 24px;
+
+    .user-info {
+      display: flex;
+      padding: 8px 16px;
+      gap: 12px;
+      align-items: center;
+      
+      .address-info {
+        flex: 1;
+        display: flex;
+        border-radius: 20px;
+        padding: 12px 10px;
+        background: rgba(255, 255, 255, 0.4);
+        align-items: center;
+        gap: 6px;
+        
+        .map-pin {
+          width: 16px;
+          height: 16px;
+        }
+        
+        .text {
+          color: colors.$Gray-90;
+          font-size: 14px;
+          font-weight: 400;
+          line-height: 20px;
+          letter-spacing: 0px;
+          text-align: left;
+        }
+      }
+      
+      .favorite {
+        padding: 4px;
+        border-radius: 20px;
+        background-color: rgba(255, 255, 255, 1);
+        font-size: 0;
+        
+        .heart {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
-
-    .address-info {
-      flex: 1;
-      text-align: center;
+    
+    .greeting {
+      padding: 8px 20px;
+      color: #fff;
+      
+      .content {
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 16px;
+      }
+      
+      .title {
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 24px;
+      }
     }
-
-    .user-avatar {
-      width: 42px;
-      height: 42px;
-      background-color: #ccc;
-      border-radius: 50%;
-      overflow: hidden;
+    
+    .search-input-wrap {
+      display: flex;
+      padding: 5px 16px;
+      margin: 0 20px;
+      box-sizing: border-box;
+      border: 1px solid colors.$Gray-30;
+      border-radius: 12px;
+      background: rgba(255, 255, 255, 1);
+      align-items: center;
+      
+      .search-img {
+        width: 20px;
+        height: 20px;
+      }
+      
+      .search-input {
+        color: colors.$Gray-80;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 20px;
+        letter-spacing: 0px;
+        text-align: left;
+      }
     }
   }
 </style>
